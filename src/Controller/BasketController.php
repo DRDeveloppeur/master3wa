ce2fa38ffa8339b8b7c92922297916aaa760f9c6
DRDeveloppeur <raul.delianu@epitech.eu>
2021-12-10 11:59:58 +0100

    feat: basket + invoice

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -3,37 +3,131 @@
 namespace App\Controller;
 
 use App\Entity\Basket;
+use App\Entity\Invoice;
 use App\Entity\Order;
 use App\Repository\BasketRepository;
+use App\Repository\DeliveryRepository;
 use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
 use App\Repository\StockRepository;
-use App\Repository\UserRepository;
+use App\Security\EmailVerifier;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Mime\Address;
 
 class BasketController extends AbstractController
 {
     private $entityManager;
+    private EmailVerifier $emailVerifier;
 
-    public function __construct(EntityManagerInterface $manager)
+    public function __construct(EntityManagerInterface $manager, EmailVerifier $emailVerifier)
     {
         $this->entityManager = $manager;
+        $this->emailVerifier = $emailVerifier;
     }
 
     /**
-     * @Route("/basket/{stock_id}/{product_id}", name="basket")
+     * @Route("/panier", name="basket")
      */
-    public function index($stock_id, $product_id, StockRepository $stockRepository, ProductRepository $productRepository, BasketRepository $basketRepository, OrderRepository $orderRepository, UserInterface $user = null, Request $request): Response
+    public function index(DeliveryRepository $deliveryRepository, BasketRepository $basketRepository): Response
+    {
+        $user = $this->getUser();
+        $deliveries = $deliveryRepository->findAll();
+        $basket = $basketRepository->findOneBy([
+            'user_id' => $user,
+            'name' => "Panier"
+        ]);
+
+        // dd($basket);
+
+        return $this->render('basket/index.html.twig', [
+            'deliveries' => $deliveries,
+            'basket' => $basket,
+        ]);
+    }
+
+    /**
+     * @Route("/less/{order_id}", name="basket_less")
+     */
+    public function less(Request $request, $order_id, OrderRepository $orderRepository): Response
+    {
+        $order = $orderRepository->find($order_id);
+        if ($order->getAmount() > 1) {
+            $order->setAmount($order->getAmount()-1);
+            $this->entityManager->persist($order);
+            $this->entityManager->flush();
+        }
+
+        return new RedirectResponse($request->headers->get('referer'));
+    }
+
+    /**
+     * @Route("/more/{order_id}", name="basket_more")
+     */
+    public function more(Request $request, $order_id, OrderRepository $orderRepository): Response
+    {
+        $order = $orderRepository->find($order_id);
+        // dd($order->getAmount(), $order->getStockId()->getAmount());
+        if ($order->getAmount() < $order->getStockId()->getAmount()) {
+            $order->setAmount($order->getAmount()+1);
+            $this->entityManager->persist($order);
+            $this->entityManager->flush();
+        } else {
+            $this->addFlash("info", "Nous en avons pas plus.");
+        }
+
+        return new RedirectResponse($request->headers->get('referer'));
+    }
+
+    /**
+     * @Route("/delete/{order_id}", name="basket_delete")
+     */
+    public function delete(Request $request, $order_id, OrderRepository $orderRepository): Response
+    {
+        $order = $orderRepository->find($order_id);
+        $basket = $order->getBasketId();
+
+        $this->entityManager->remove($order);
+        $this->entityManager->flush();
+
+        if (count($basket->getOrders()) === 0) {
+            $this->entityManager->remove($basket);
+            $this->entityManager->flush();
+        }
+        
+        $this->addFlash("info", "Le produit '".$order->getProductId()->getModel()."' à été enlevé du panier.");
+        
+        return new RedirectResponse($request->headers->get('referer'));
+    }
+
+    /**
+     * @Route("/delivery/{basket_id}/{delivery_id}", name="basket_choice_delivery")
+     */
+    public function choiceDelivery(Request $request, $basket_id, $delivery_id, BasketRepository $basketRepository, DeliveryRepository $deliveryRepository): Response
+    {
+        $basket = $basketRepository->find($basket_id);
+        $delivery = $deliveryRepository->find($delivery_id);
+        $basket->setDeliveryId($delivery);
+        $this->entityManager->persist($basket);
+        $this->entityManager->flush();
+
+        return new RedirectResponse($request->headers->get('referer'));
+    }
+
+    /**
+     * @Route("/basket/{stock_id}/{product_id}", name="basket_add")
+     */
+    public function add($stock_id, $product_id, StockRepository $stockRepository, ProductRepository $productRepository, BasketRepository $basketRepository, OrderRepository $orderRepository, Request $request): Response
     {
         $product = $productRepository->find($product_id);
         $stock = $stockRepository->find($stock_id);
+        $user = $this->getUser();
         
         if ($user) {
             $user_basket = $basketRepository->findOneBy([
@@ -80,5 +174,45 @@ class BasketController extends AbstractController
         }
 
         return new RedirectResponse($request->headers->get('referer'));
+    }
+
+    /**
+     * @Route("/payed/{basket_id}", name="basket_pay")
+     */
+    public function payed(Request $request, $basket_id, BasketRepository $basketRepository): Response
+    {
+        $user = $this->getUser();
+
+        $now = new DateTime('now');
+        $basket = $basketRepository->find($basket_id);
+        $basket->setName("Facture");
+        $basket->setIsPayed(true);
+        $this->entityManager->persist($basket);
+
+        $invoice = new Invoice();
+        $invoice->setBasketId($basket);
+        $invoice->setName("Facture");
+        $invoice->setCreatedAt($now);
+        $this->entityManager->persist($invoice);
+        
+        $this->entityManager->flush();
+
+        if ($user && $user->isVerified()) {
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('raul3wa@gmail.com', 'Carrée de la mode'))
+                    ->to($user->getEmail())
+                    ->subject('Confirmation de commande')
+                    ->htmlTemplate('emails/confirm_cmde.html.twig')
+                    ->context([
+                        'user' => $user,
+                        'invoice' => $invoice,
+                    ])
+            );
+        }
+        
+        $this->addFlash("success", "Payement effectuer. Vous recevrez votre facture par mail à l'adresse ".$basket->getUserId()->getEmail().".");
+
+        return $this->redirectToRoute('home');
     }
 }
